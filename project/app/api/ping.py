42557e2c7a1176d08c0122fdebf10680616fae01
Vinícius Aguiar <viniciusoa@protonmail.com>
2023-06-26 17:46:20 -0300

    refactor: added pydantic response schema.

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Depends
 
 from app.config import get_settings, Settings
+from app.models.pydantic import PongResponseSchema
 
 router = APIRouter()
 
 
 @router.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing
-    }
+async def pong(settings: Settings = Depends(get_settings)) -> PongResponseSchema:
+    return PongResponseSchema(
+        ping="pong!",
+        environment=settings.environment,
+        testing=settings.testing
+    )
